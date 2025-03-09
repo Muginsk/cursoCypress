51182d24860cf52baa9b42c51d6d1e754cc43951
Felipe Muginsk <felipe.muginsk@gmail.com>
2025-03-09 20:59:28 -0300

    subindo testes atualizados

--- cypress/e2e/login/loginGmail.cy.js
+++ cypress/e2e/login/loginGmail.cy.js
@@ -1,10 +1,15 @@
-/// <reference types="cypress" />
+describe('testes no site saucedemo', () => {
 
-describe('validar login gmail', () => {
-  it('deve realizar o login no gmail', () => {
-    cy.visit('https://www.gmail.com')
-    cy.get('#identifierId').type('felipe.muginsk')
-    cy.get('(//span[@jsname="V67aGc"])[2]').click()
-    
+  it('Realiza login e valida que está na página de produtos', () => {
+    cy.visit('https://www.saucedemo.com/v1')
+
+    // Preenchendo os campos de login
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    // Validando que o login foi bem-sucedido e estamos na página correta
+    cy.url().should('include', '/inventory.html')
+    cy.get('.product_label').should('be.visible').and('contain.text', 'Products')
   })
 })
